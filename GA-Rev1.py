8fa657bf334f9cc424282bd198fdf261fedb50f7
atikazzahra <atikazzahraa@gmail.com>
2016-09-22 17:24:42 +0700

    HILL

--- GA-Rev1.py
+++ GA-Rev1.py
@@ -55,6 +55,31 @@ def createPopulation(list_jadwal, list_ruangan):
 		population.append([species,checkConstraint(species, list_ruangan)])
 	return population
 
+# Validasi conflict untuk nyari neighboor
+def conflictHariMatkul(matkul1, matkul2):
+	if (matkul1.hari == matkul2.hari):
+		return True
+	else:
+		return False
+
+def conflictJamMatkul(matkul1, matkul2):
+	if (matkul1.jam_mulai >= matkul2.jam_mulai and matkul1.jam_mulai < matkul2.jam_mulai+matkul2.durasi) or (matkul2.jam_mulai >= matkul1.jam_mulai and matkul2.jam_mulai < matkul1.jam_mulai+matkul1.durasi):
+		return True
+	else:
+		return False
+
+def conflictHariRuangan(matkul, ruangan):
+	if (matkul.hari not in ruangan.hari):
+		return True
+	else:
+		return False
+
+def conflictJamRuangan(matkul, ruangan):
+	if (matkul.jam_mulai < ruangan.jam_mulai) or (matkul.jam_mulai+matkul.durasi > ruangan.jam_akhir):
+		return True
+	else:
+		return False
+
 def checkConstraintMatkul(matkul1, matkul2):
 	constraintBroken = 0
 	if (matkul1.hari == matkul2.hari):
@@ -94,6 +119,56 @@ def copySpecies(species):
 		new_species.append(copy.copy(species[i]))
 	return new_species
 	
+#HILL CLIMBING
+def highestValueNeighboor(list_matkul, list_ruangan):
+	successor = copySpecies(list_matkul)
+	for i in range(len(list_matkul)):
+		for j in range(i+1, len(list_matkul)):
+			if checkConstraintMatkul(list_matkul[i], list_matkul[j]) != 0:
+				if conflictHariMatkul(list_matkul[i], list_matkul[j]):
+					while (successor[i].hari == successor[j].hari):
+						print ("a")
+						successor[i].hari = successor[i].domain_hari[random.randrange(0,len(successor[i].domain_hari))]
+				elif conflictJamMatkul(list_matkul[i], list_matkul[j]):
+					while (successor[i].jam_mulai == successor[j].jam_mulai):
+						print successor[i].domain_jam_mulai
+						print successor[i].kode
+						print ("b", successor[i].jam_mulai)
+						successor[i].jam_mulai = successor[i].domain_jam_mulai[random.randrange(0,len(successor[i].domain_jam_mulai))]
+		
+		if checkConstraintRuang(successor[i], list_ruangan) != 0:
+			ruangan = 0
+			for ruang in list_ruangan:
+				if (ruang.kelas == list_matkul[i].kelas):
+					ruangan = ruang
+					break
+
+			if conflictHariRuangan(list_matkul[i], ruangan):
+				if sameValueinLists(ruangan.hari, successor[i].domain_hari):
+					while (successor[i].hari not in ruangan.hari):
+						print successor[i].domain_hari
+						print successor[i].kode
+						print ("c", successor[i].hari)
+						successor[i].hari = successor[i].domain_hari[random.randrange(0,len(successor[i].domain_hari))]
+				else: 
+					successor[i].kelas = successor[i].domain_kelas[random.randrange(0,len(successor[i].domain_kelas))]
+			if conflictJamRuangan(successor[i], ruangan):
+				if ruangan.jam_mulai in successor[i].domain_jam_mulai:
+					while conflictJamRuangan(successor[i], ruangan):
+						print successor[i].domain_jam_mulai
+						print successor[i].kode
+						print ("d", successor[i].jam_mulai, ruangan.jam_mulai)
+						successor[i].jam_mulai = successor[i].domain_jam_mulai[random.randrange(0,len(successor[i].domain_jam_mulai))]
+				else: 
+					successor[i].kelas = successor[i].domain_kelas[random.randrange(0,len(successor[i].domain_kelas))]	
+	return successor
+
+def sameValueinLists(list1, list2):
+	for x in list1:
+		if x in list2:
+			return True
+	return False
+
 def fitnessTest(populasi):
 	fitSpecies = []
 	populasi.sort(key=lambda x: x[1])
@@ -121,11 +196,11 @@ def readFile(filename):
 	with open(filename) as file:
 		r = 0
 		for line in file:
-			if (line == 'Ruangan\n'):
+			if (line == 'Ruangan\r\n'):
 				r = 1
-			elif (line == 'Jadwal\n'):
+			elif (line == 'Jadwal\r\n'):
 				r = 2
-			elif (line == '\n'):
+			elif (line == '\r\n'):
 				pass
 			elif (r == 1):
 				ltemp = line[:-1].split(';')
@@ -138,6 +213,7 @@ def readFile(filename):
 	return listRuangan, listJadwal
 
 def main():
+	print("GA")
 	listRuangan, listJadwal = readFile('Testcase.txt')
 	fitSpecies = []
 	for i in range(50):
@@ -163,6 +239,20 @@ def main():
 		fittestSpecies[0][i].print_jadwal()
 	print 'Constraint broken: ' + str(fittestSpecies[1])
 	print 'Constraint broken: ' + str(checkConstraint(fittestSpecies[0], listRuangan))
+
+	print("HILL")
+	problem = createSpecies(listJadwal, listRuangan)
+	current = copySpecies(problem)
+	neighboor = highestValueNeighboor(current, listRuangan)
+	print "current:", checkConstraint(current, listRuangan), "neighboor:", checkConstraint(neighboor, listRuangan)
+	while(checkConstraint(current, listRuangan) > checkConstraint(neighboor, listRuangan)):
+		print "current:", checkConstraint(current, listRuangan), "neighboor:", checkConstraint(neighboor, listRuangan)
+		current = copySpecies(neighboor)
+		neighboor = highestValueNeighboor(current, listRuangan)
+	problem = copySpecies(current)
+	for i in range(len(problem)):
+		problem[i].print_jadwal()
+	print 'constraintBroken: ' + str(checkConstraint(problem, listRuangan))
 
 if __name__ == '__main__':
 	main()
